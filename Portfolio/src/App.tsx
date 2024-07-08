e6e3200121eb52c67676f28904700d8d9b10548b
Juanliendo22 <liendojuan070@gmail.com>
2024-07-08 17:07:40 -0300

    PRIMER COMMIT

--- Portfolio/src/App.tsx
+++ Portfolio/src/App.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import Nav from "./Componentes/Nav.jsx";
 import Main from "./Componentes/Main.jsx"; // Corrige la importación para que coincida con el archivo
 import Contenido from "./Componentes/Contenido.jsx"; // Corrige la importación para que coincida con el archivo
+import Footer from "./Componentes/Footer.jsx";
+import Projects from "./Componentes/Projects.jsx";
 
 const App = () => {
   return (
@@ -10,6 +12,8 @@ const App = () => {
       <Nav />
       <Main />
       <Contenido />
+      <Projects />
+      <Footer />
     </div>
   );
 };
---
+++ Portfolio/src/Componentes/Projects.jsx
@@ -0,0 +1,20 @@
+// src/components/Footer.jsx
+
+import React from "react";
+import "./Projects.css"; // Importa los estilos para el footer
+
+const Projects = () => {
+  return (
+    <div className="ProjectsMain">
+      <h3>My projects</h3>
+      <div className="ContenedorCajas">
+        <div className="Caja"></div>
+        <div className="Caja"></div>
+        <div className="Caja"></div>
+        <div className="Caja"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
