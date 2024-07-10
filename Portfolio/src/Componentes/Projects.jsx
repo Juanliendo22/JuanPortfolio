a388c2f2f54a12ac97b4cc3db700daaaed7618f8
Juanliendo22 <liendojuan070@gmail.com>
2024-07-10 12:06:32 -0300

    cambios variados -navbar -imagen principal -proyectos pulidos

--- Portfolio/src/Componentes/Projects.jsx
+++ Portfolio/src/Componentes/Projects.jsx
@@ -2,16 +2,46 @@
 
 import React from "react";
 import "./Projects.css"; // Importa los estilos para el footer
+import tuwebi from "../assets/tuwebi.png";
+import Ae from "../assets/Ae.png";
+import okto from "../assets/okto.png";
 
 const Projects = () => {
+  const projects = [
+    {
+      id: 1,
+      img: tuwebi,
+      alt: "Tuwebi Project",
+      description:
+        "Consiste en la pagina web de un emprendimiento que crea paginas web, del cual soy una de las personas que forman parte del proyecto",
+    },
+    {
+      id: 2,
+      img: okto,
+      alt: "Okto Project",
+      description:
+        "Fue una pagina para un cliente real de TuWebi, la cual tuve que realizar desde 0",
+    },
+    {
+      id: 3,
+      img: Ae,
+      alt: "Ae Project",
+      description: "Consiste en una herramienta para AmericanExpress",
+    },
+  ];
+
   return (
     <div id="projects" className="ProjectsMain">
       <h3>My projects</h3>
       <div className="ContenedorCajas">
-        <div className="Caja"></div>
-        <div className="Caja"></div>
-        <div className="Caja"></div>
-        <div className="Caja"></div>
+        {projects.map((project) => (
+          <div key={project.id} className="Caja">
+            <img src={project.img} alt={project.alt} />
+            <div className="Description">
+              <p>{project.description}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
